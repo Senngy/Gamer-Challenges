0672b55d166407b7696da917fdc8c9ee9875fa7b
Senngy <sylvainmarchal@hotmail.com>
2025-07-03 18:25:07 +0200

    MVC organisation & package.json update

---
+++ api/app.js
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import express from "express";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port at http://localhost:${PORT}`);
+});
