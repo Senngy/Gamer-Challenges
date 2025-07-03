5a42f1b875826f3a47ac7ad5921e8c0206cbd0bd
Senngy <sylvainmarchal@hotmail.com>
2025-07-03 21:13:59 +0200

    update v2 Routes & MVC

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,5 +1,11 @@
+// auth.routes.js
 import express from 'express';
 
 const router = express.Router();   
 
-router.get('/me',);
+router.get('/me',); // Get the current user PRIVATE
+router.post('/login',); // Login a user
+router.post('/register',); //  Register a new user (signup dans le cahier des charges)
+router.post('/logout',); // Logout a user PRIVATE
+
+export default router;
---
+++ api/routes/games.routes.js
@@ -0,0 +1,9 @@
+// games.routes.js
+import express from "express";
+
+const router = express.Router();
+
+router.get('/',) // All the games
+router.get('/:id') // Game detail
+
+export default router;
