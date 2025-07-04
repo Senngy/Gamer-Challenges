f8b8d68732edfd915455a1380d39eb7aabb33c0a
Senngy <sylvainmarchal@hotmail.com>
2025-07-04 17:37:26 +0200

    Update layout conflict

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,12 +1,7 @@
 <script>
-  import '../reset.css';
-  import '../global.css';
+
 </script>
 
-<!-- HEADER -->
-<header class="header">
-
-</header>
 <main>
   <slot /> <!-- contenu de chaque page -->
 </main>
