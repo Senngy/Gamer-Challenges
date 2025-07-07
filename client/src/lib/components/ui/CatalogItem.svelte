4d9360d42b3b724a1222779a68b96e46f624e5a6
Thomas <brelotthomas@gmail.com>
2025-07-07 18:36:28 +0200

    corection bug for page accueil

--- client/src/lib/components/ui/CatalogItem.svelte
+++ client/src/lib/components/ui/CatalogItem.svelte
@@ -22,22 +22,6 @@
   <div class="catalog__content">
     <div class="catalog__tags">
       <span class="catalog__tag">{game.platform}</span>
-
-<div class="catalog__item" role="listitem" aria-label="Apex Legends, Saison 5, Stratégie, Battle Royale">
-    <div class="catalog__image" role="img" aria-label="Illustration Apex Legends">
-        <div class="catalog__like-count" aria-label="Nombre de challenges">
-            #nbChallenges
-        </div>
     </div>
-    <div class="catalog__content">
-        <div class="catalog__tags">
-            <span class="catalog__tag">#categoryTag1</span>
-            <span class="catalog__tag">#categoryTag2</span>
-        </div>
-        <h3 class="catalog__game-name">#gameTitle</h3>
-
-    </div>
-    <h3 class="catalog__game-name">{game.title}</h3>
-    <p class="catalog__season">{game.description}</p>
-  </div>
+    </div>  
 </div>
