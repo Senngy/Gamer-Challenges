ca80c12cd20ce1a7ae1b9aa86da8648a73741987
Thomas <brelotthomas@gmail.com>
2025-07-11 22:37:03 +0200

    debug and affichage slider dynamique, create route

--- api/routes/games.routes.js
+++ api/routes/games.routes.js
@@ -1,15 +1,20 @@
 import express from 'express';
-import { getAll, getById, getChallengesByGameId } from '../controllers/games.controller.js';
+import { getAll, getById, getChallengesByGameId, getTopGames } from '../controllers/games.controller.js';
 
 const router = express.Router();
 
 // Route pour récupérer tous les jeux
 router.get('/', getAll);
 
+// route pour recuperer le top 3 des jeux via leur participation
+router.get('/top', getTopGames);
+
 // Route pour récupérer un jeu par ID
 router.get('/:id', getById);
 
 // Route pour récupérer les challenges d'un jeu spécifique
 router.get('/:id/challenges', getChallengesByGameId);
+
+
 
 export default router;
