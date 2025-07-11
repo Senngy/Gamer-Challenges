e6f9bfba36aa6517cedbd44d7f1c72cb9729aea1
Thomas <brelotthomas@gmail.com>
2025-07-11 11:29:31 +0200

    amelioration de la page d'accueil + dynamisation

--- client/src/routes/games/+page.svelte
+++ client/src/routes/games/+page.svelte
@@ -1,25 +1,25 @@
 <script>
-    import CatalogItem from '$lib/components/ui/CatalogItem.svelte';
-    // export let data;
-    // let games = data.games;
-    const { data } = $props();
-    const { games } = data;
-    
-    let visibleCount = $state(6);
-  </script>
-  
-  <section class="catalog" aria-labelledby="catalog-title">
-    <h1 class="catalog__title">Choisissez un jeu et lancez vous dans un défi !</h1>
-    <div class="catalog__grid" role="list">
-      {#each games.slice(0, visibleCount) as game}
-        <CatalogItem {game} />
-      {/each}
-    </div>
-    {#if visibleCount < games.length}
-      <div class="load-more-container">
-        <button class="btn" id="load-more" on:click={() => visibleCount += 6}>
-          Voir plus de jeux
-        </button>
-      </div>
-    {/if}
-  </section>
+	import CatalogItem from '$lib/components/ui/CatalogItem.svelte';
+	// export let data;
+	// let games = data.games;
+	const { data } = $props();
+	const { games } = data;
+
+	let visibleCount = $state(6);
+</script>
+
+<section class="catalog" aria-labelledby="catalog-title">
+	<h1 class="catalog__title">Choisissez un jeu et lancez vous dans un défi !</h1>
+	<div class="catalog__grid" role="list">
+		{#each games.slice(0, visibleCount) as game}
+			<CatalogItem {game} />
+		{/each}
+	</div>
+	{#if visibleCount < games.length}
+		<div class="load-more-container">
+			<button class="btn" id="load-more" onclick={() => (visibleCount += 6)}>
+				Voir plus de jeux
+			</button>
+		</div>
+	{/if}
+</section>
