239b625f7eda665c89b70ca6b50603521709ef40
SamuelDuthoitOclock <samuel.duthoit@oclock.school>
2025-07-11 15:47:04 +0200

    setup of the authentification on the challenge and participation creation routes

--- api/routes/challenges.routes.js
+++ api/routes/challenges.routes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 
-import { getAll, getById, getChallengeParticipations, create } from '../controllers/challenges.controller.js'; // Import des fonctions du controller
-// import { authenticate } from '../middlewares/auth.middleware.js';
+import { authenticate } from '../middlewares/auth.middleware.js';
+
+import { getAll, getById, getChallengeParticipations, create } from '../controllers/challenges.controller.js';
+
 import { validateUserCreationChallenge } from '../middlewares/challenge.middleware.js';
 
 const router = express.Router();
@@ -9,6 +11,6 @@ const router = express.Router();
 router.get('/', getAll); // Récupérer tous les challenges
 router.get('/:id', getById); // Récupérer un challenge spécifique par son ID
 router.get('/:id/participations',  getChallengeParticipations); // Récupérer les participations d'un challenge
-router.post('/', create); // Créer un nouveau challenge // Ne pas oublier d'implémenter la fonction authenticate (route privée)
+router.post('/', authenticate, validateUserCreationChallenge, create); // Authentification TESTÉE & VALIDÉE - Créer un nouveau challenge
 
 export default router;
