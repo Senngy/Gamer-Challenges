0d4937db5eedad2ce5441a259b1eaf37918fb041
SamuelDuthoitOclock <samuel.duthoit@oclock.school>
2025-07-15 15:35:25 +0200

    Design overhaul of the challenge detail page and improvement of the like feature

--- client/src/lib/components/ui/LikeItemParticipaton.svelte
+++ client/src/lib/components/ui/LikeItemParticipaton.svelte
@@ -28,6 +28,9 @@
         console.log('CLASSCSS', classCSS)
         try {
             const { likedNow } = await toggleLike(participation.id); // Nous renvois true si l'utilisateur n'a pas encore liké et ajoute un like sinon supprime le like
+
+            liked = likedNow;
+
             if (likedNow) {
                 likes += 1;
             }
@@ -73,32 +76,78 @@
 
 </script>
 
-<button type="button" class={`like-button ${classCSS}`} on:click={handleToggleLike}>
-    ❤️ <span class="like-count">{likes}</span>
+<button type="button" class={`like-button ${classCSS}`} class:liked={liked} on:click={handleToggleLike}>
+    <span class="like-count">{likes}</span>
 </button>
 
 <style>
-    .like-button {
-        background: none;
-        border: 1px solid #ccc;
-        padding: 8px 12px;
-        font-size: 1rem;
-        cursor: pointer;
-        border-radius: 6px;
-        transition: all 0.2s;
-    }
+.like-button {
+    background-color: rgba(255, 255, 255, 0.1);
+    border: 1px solid #ccc;
+    padding: 8px 12px;
+    font-size: 1rem;
+    cursor: pointer;
+    border-radius: 20px;
+    transition: all 0.2s;
+    width: fit-content;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
-    .like-count {
-        margin-left: 6px;
-    }
+  .like-button::before {
+    content: "";
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    margin-right: 6px;
+    background-image: url('/images/icons/heart-50-outlined.png');
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
 
-    .like-button:disabled {
-        opacity: 0.6;
-        cursor: wait;
+  .like-button:hover {
+    background: var(--btn-color);
+  }
+
+  .liked {
+    color: var(--text-color-red);
+    animation: pulse 0.2s ease-in-out;
+  }
+
+  .like-button.liked::before {
+    content: "";
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    margin-right: 6px;
+    background-image: url('/images/icons/heart-50-filled.png');
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+
+  @keyframes pulse {
+    0% {
+      transform: scale(1);
     }
-    .btn-from-participation{
-        min-width: 200px;
-        color:white;
-        margin: 1rem auto; 
+    50% {
+      transform: scale(1.2);
     }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  .like-count {
+    margin-left: 6px;
+  }
+
+  .like-button:disabled {
+    opacity: 0.6;
+    cursor: wait;
+  }
+  .btn-from-challenge-page, .btn-from-challenge-item, .btn-from-participation {
+		min-width: 100px;
+		color:white;
+  }
 </style>
