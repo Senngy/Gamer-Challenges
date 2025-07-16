4588f9d25e3bd542abe5646aef5a37ba8b8b7369
SamuelDuthoitOclock <samuel.duthoit@oclock.school>
2025-07-16 12:02:25 +0200

    review of the gameId server page

--- client/src/routes/games/[gameId]/+page.server.js
+++ client/src/routes/games/[gameId]/+page.server.js
@@ -1,13 +1,8 @@
 // src/routes/games/[gameId]/+page.server.js
-import { error, HttpError } from '@sveltejs/kit';
-// import api from '$lib/api.js';
-
+import { error } from '@sveltejs/kit';
 
 export async function load({ fetch, params }) {
   const { gameId } = params;
-  console.log(gameId);
-  
-
 
   if (!gameId || isNaN(+gameId)) {
     throw error(400, 'Paramètre gameId manquant ou invalide');
@@ -16,19 +11,18 @@ export async function load({ fetch, params }) {
   try {
     const [gameRes, challengesRes] = await Promise.all([
       fetch(`http://localhost:3000/games/${gameId}`),
-      fetch(`http://localhost:3000/games/${gameId}/challenges`) // CHANGER LE FETCH POUR LES CHALLENGES D'UN JEU
+      fetch(`http://localhost:3000/games/${gameId}/challenges`)
     ]);
 
-    if (!gameRes.ok) throw error(gameRes.status, 'Jeu introuvable');
+    if (!gameRes.ok) {
+      throw error(gameRes.status, 'Jeu introuvable');
+    }
 
     const game = await gameRes.json();
     const challenges = challengesRes.ok ? await challengesRes.json() : [];
-    console.log('challenges from +page.server.js:', challenges);
 
     return { game, challenges };
   } catch (err) {
-    if (err instanceof HttpError) throw err;
-
     console.error('Erreur dans +page.server.js :', err);
     throw error(500, 'Erreur lors du chargement des données');
   }
