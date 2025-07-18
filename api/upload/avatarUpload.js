cd0e5d7b9e8ec85f6500647c618f926ab6f54d73
SamuelDuthoitOclock <samuel.duthoit@oclock.school>
2025-07-18 10:19:06 +0200

    inclusion of the gif file type for the avatar upload

--- api/upload/avatarUpload.js
+++ api/upload/avatarUpload.js
@@ -20,7 +20,7 @@ const storage = multer.diskStorage({
 
 // Filtrer les types de fichiers
 const fileFilter = (req, file, cb) => {
-  const allowedTypes = ['image/jpeg', 'image/png', 'image/webp'];
+  const allowedTypes = ['image/gif', 'image/jpeg', 'image/png', 'image/webp'];
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
